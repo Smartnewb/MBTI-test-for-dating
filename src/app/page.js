b7c8abb5386b30ea40f1873604d1ca9e6ce99609
JUNYEONG <smartnewb2@gmail.com>
2025-05-18 16:31:26 +0900

    Fix: Update page.js comments to clarify metadata location

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import HomePage from './components/HomePage';
  *
  * 달빛 연애 연구소의 메인 랜딩 페이지입니다.
  * 신비로운 타로 테마를 가진 매력적인 인트로 페이지를 제공합니다.
+ *
+ * 메타데이터는 layout.js에서 정의됩니다.
  */
 export default function Home() {
   return <HomePage />;
